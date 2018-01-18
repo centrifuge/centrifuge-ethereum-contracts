a97e730545537b61d3b51b8ef0ef97b8be605252
Lucas Vogelsang <l@lucasvo.com>
2018-01-18 10:01:03 -0500

    Remove old comment

--- contracts/Witness.sol
+++ contracts/Witness.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.17;
 
 contract Witness {
-    // Why address: address is the only longer fixed length type that can be used in mapping. Otherwise we'd have to create a lookup table with mapping (bye32 => mapping (byte32 => bytes))
     mapping (bytes32 => bytes32[2]) public witness_list;
 
     function witnessDocument (bytes32 version, bytes32 merkleRoot) public {
