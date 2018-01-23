a03d99e3bc951dba9fbebcd75315cafd6d1232c0
Lucas Vogelsang <l@lucasvo.com>
2018-01-23 07:24:17 -0800

    Changing version to identifier

--- contracts/Witness.sol
+++ contracts/Witness.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.17;
 contract Witness {
     mapping (bytes32 => bytes32[2]) public witness_list;
 
-    function witnessDocument (bytes32 version, bytes32 merkleRoot) public {
-        require(witness_list[version][0] == 0x0);
-        witness_list[version] = [merkleRoot, bytes32(now)];
+    function witnessDocument (bytes32 identifier, bytes32 merkleRoot) public {
+        require(witness_list[identifier][0] == 0x0);
+        witness_list[identifier] = [merkleRoot, bytes32(now)];
     }
 
-    function getWitness (bytes32 version) public view returns(bytes32[2]) {
-        return witness_list[version];
+    function getWitness (bytes32 identifier) public view returns(bytes32[2]) {
+        return witness_list[identifier];
     }
 }
--- test/Witness.test.js
+++ test/Witness.test.js
@@ -1,39 +1,39 @@
 var Witness = artifacts.require("Witness");
 
 function createRandomByte32() {
-    let version = '';
+    let identifier = '';
     for (var i = 0; i < 64; i++) {
-        version += Math.floor(Math.random()*16).toString(16)
+        identifier += Math.floor(Math.random()*16).toString(16)
     }
-    return '0x'+version
+    return '0x'+identifier
 }
 
 contract("Witness", function (accounts) {
-    const version = createRandomByte32()
-    const nextVersion = createRandomByte32()
+    const identifier = createRandomByte32()
+    const nextidentifier = createRandomByte32()
     const merkleRoot = createRandomByte32()
     it("should write a merkle root to the chain", async function  () {
         let witness = await Witness.deployed()
         
-        await witness.witnessDocument(version, merkleRoot, {from:accounts[0]}).then(function (tx) {
+        await witness.witnessDocument(identifier, merkleRoot, {from:accounts[0]}).then(function (tx) {
             assert.equal(tx.receipt.status, 1)
             return tx
         }).catch(function (e) {
             throw e
         })
         
-        let response = await witness.getWitness.call(version, {from: accounts[0]})
+        let response = await witness.getWitness.call(identifier, {from: accounts[0]})
         assert.equal(merkleRoot, response[0])
 
         // Try overwriting an existing merkleRoot
-        await witness.witnessDocument(version, merkleRoot.substr(2), {from:accounts[0]}).then(function (tx) {
+        await witness.witnessDocument(identifier, merkleRoot.substr(2), {from:accounts[0]}).then(function (tx) {
             throw "Transaction should not succeed"
         }).catch(function (e) {
             assert.equal(e.message, "VM Exception while processing transaction: revert")
         })
 
         // Ensure merkleRoot hasn't changed
-        response = await witness.getWitness.call(version, {from: accounts[0]})
+        response = await witness.getWitness.call(identifier, {from: accounts[0]})
         assert.equal(merkleRoot, response[0])
 
         // TODO: Check blocktime
