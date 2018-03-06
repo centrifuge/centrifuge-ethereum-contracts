6555b5a59701126f7ec4d350a76f7408d03d6bba
Philip Stehlik <p@pstehlik.com>
2018-03-06 09:38:25 -0800

    local geth network in config

--- truffle.js
+++ truffle.js
@@ -7,11 +7,18 @@ module.exports = {
         from: "0xd77c534aed04d7ce34cd425073a033db4fbe6a9d",
         gas: 4712388
       },
+      localgeth: {
+        host: "localhost",
+        port: 9545,
+        network_id: "*", // Match any network id
+        from: "0x4b1b843af77a8f7f4f0ad2145095937e3d90e3d8",
+        gas: 4712388
+      },
       rinkeby: {
           host: "localhost",
-          port: 8545,
+          port: 8546,
           network_id: "4", // rinkeby network ID
-          from: "0x838f7dcA284eb69A9c489fE09c31cFf37DeFDEcA", // default address to use for any transaction Truffle makes during migrations
+          from: "0x96281d1d54dcdd6a68be56ba45adcc9f8ad82e44", // default address to use for any transaction Truffle makes during migrations
           gas: 4612388 // Gas limit used for deploys
         }
     }
