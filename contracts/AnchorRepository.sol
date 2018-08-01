387a6b5a81e216da30ae22b812491a3f67360e84
razvan <razvan@centrifuge.io>
2018-08-01 18:23:28 +0200

    bug fixes and add AnchorRepository to deployment scrips

--- contracts/AnchorRepository.sol
+++ contracts/AnchorRepository.sol
@@ -12,11 +12,11 @@ contract AnchorRepository  {
         uint centrifugeId;
     }
 
-    struct PreCommit {
+    struct PreAnchor {
         uint anchodId;
-        uint signatureRoot,
-        uint centrifugeId,
-        uint expirationBlock,
+        uint signatureRoot;
+        uint centrifugeId;
+        uint expirationBlock;
     }
 
     mapping (uint => PreCommit) public preCommits;
@@ -24,30 +24,26 @@ contract AnchorRepository  {
     mapping (uint => Anchor) public commits;
 
 
-    function getExpirationLegth() returns (uint256) {
-        return 15;
-    }
-
-    function preCommit (uint256 _anchorId, uint256 _singningRoot, uint256 _centId) external payable {
+    function preCommit (uint256 _anchorId, uint256 _singningRoot, uint256 _centrifugeId) external payable {
 
         // do not allow a precommit if there is already a valid one in place
-        require(hasPreCommit(_anchorId) == false);
+        require(hasValidPreCommit(_anchorId) == false);
 
         // TODO check if msg.sender is authorized to act on behalf of the centId
 
-        preCommits[_anchorId] = PreCommit(_anchorId, _singningRoot, _centId, block.number + getExpirationLegth());
-        AnchorPreCommited(msg.sender, _anchorId, _singningRoot, _centId, now );
+        preCommits[_anchorId] = PreAnchor(_anchorId, _singningRoot, _centrifugeId, block.number + 15);
+        emit AnchorPreCommited(msg.sender, _anchorId, _singningRoot, _centrifugeId, now );
 
     }
 
     // Check if there is a valid precommit for an anchorID
-    function hasPreCommit(uint256 _anchorId) external view returns (bool) {
+    function hasValidPreCommit(uint256 _anchorId) public view returns (bool) {
            return (preCommits[_anchorId].expirationBlock != 0x0 && preCommits[_anchorId].expirationBlock < block.number);
     }
 
-    function commit (uint256 _anchorId, uint256 _documentRoot, uint _centId) external payable {
+    function commit (uint256 _anchorId, uint256 _documentRoot, uint _centrifugeId) external payable {
         // in order to commit we must insure a pre commit has been done before
-        require(hasPreCommit(_anchorId) == true);
+        require(hasValidPreCommit(_anchorId) == true);
 
         // TODO check if msg.sender is authorized to act on behalf of the centId
 
@@ -65,16 +61,16 @@ contract AnchorRepository  {
         require(_anchorId != 0x6e756c6c00000000000000000000000000000000000000000000000000000000);
         require(_documentRoot != 0x6e756c6c00000000000000000000000000000000000000000000000000000000);
 
-        timestamp = now;
+        uint timestamp = now;
         commits[_anchorId] = Anchor(_anchorId, _documentRoot, timestamp, _centrifugeId);
-        AnchorCommited(msg.sender, _anchorId, _documentRoot, timestamp, _centrifugeId);
+        emit AnchorCommited(msg.sender, _anchorId, _documentRoot, timestamp, _centrifugeId);
 
     }
 
     function getAnchorById (uint256 _anchorId) public view returns(uint256, uint256, uint256, uint256) {
         return (
-            commits[_anchorId]._anchorId,
-            commits[_anchorId]._documentRoot,
+            commits[_anchorId].id,
+            commits[_anchorId].documentRoot,
             commits[_anchorId].timestamp,
             commits[_anchorId].centrifugeId
             );
