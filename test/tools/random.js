16842bf41b45851553cd74ae92c1b8f3d290a118
razvan <razvan@centrifuge.io>
2018-08-07 15:12:02 +0200

    fix typos

--- test/tools/random.js
+++ test/tools/random.js
@@ -9,7 +9,7 @@ module.exports = {
     createRandomByte: function(max = 32) {
         let identifier = '';
         for (var i = 0; i < max*2; i++) {
-            //  avoid  a hash that start with 0x0 as web3.toHex  removes it and test may randomly fail
+            //  avoid a hash that starts with 0x0 as web3.toHex removes it and tests may randomly fail
             identifier += Math.floor(Math.random() * 15  + 1).toString(16)
         }
         return '0x' + identifier
