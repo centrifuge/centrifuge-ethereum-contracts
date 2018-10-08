45e2b0211888d1b6c742f6458f3e4742e6aa7370
razvan <razvan@centrifuge.io>
2018-10-08 16:24:43 +0200

    remove promise.all

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,9 +6,7 @@ const AnchorRepository = artifacts.require("AnchorRepository");
 module.exports = function (deployer) {
     deployer.deploy(AnchorRegistry);
     deployer.deploy(IdentityRegistry).then(function (reg) {
-        return Promise.all([
-            deployer.deploy(AnchorRepository, IdentityRegistry.address),
-            deployer.deploy(IdentityFactory, IdentityRegistry.address)
-        ])
+        deployer.deploy(IdentityFactory, IdentityRegistry.address)
+        return deployer.deploy(AnchorRepository, IdentityRegistry.address);
     });
 };
