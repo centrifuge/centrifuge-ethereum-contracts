c3e2bfae4e2e75852a9e559fb279a60eda2ed6f1
razvan <razvan@centrifuge.io>
2018-10-09 11:04:46 +0200

    test travis flow

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -15,7 +15,7 @@ if [ "$#" -ne 1 ]; then
   usage
 fi
 
-if [[ ! "$1" =~ ^(local|integration|rinkeby)$ ]]; then
+if [[ ! "$1" =~ ^(localgeth|integration|rinkeby)$ ]]; then
     echo "Environment [${1}] not allowed"
     usage
 fi
@@ -28,13 +28,19 @@ fi
 MIGRATE_ADDRESS=${MIGRATE_ADDRESS:-'0x89b0a86583c4444acfd71b463e0d3c55ae1412a5'}
 MIGRATE_PASSWORD=${MIGRATE_PASSWORD:-''}
 
-if [[ "$1" =~ ^(local|integration)$ ]]; then
+rm -Rf $local_dir/../build
+if [[ "$1" =~ ^(localgeth|integration)$ ]]; then
+
   docker run -it --net=host --entrypoint "/geth" centrifugeio/cent-geth:v0.1.0 attach http://localhost:9545 --exec "personal.unlockAccount('${MIGRATE_ADDRESS}', '${MIGRATE_PASSWORD}', 500)"
 fi
 
-rm -Rf $local_dir/../build
+if [[ "$1" =~ ^(rinkeby)$ ]]; then
+  npm install centrifuge-ethereum-contracts@latest --force
+  cp -rf $local_dir/../node_modules/centrifuge-ethereum-contracts/build $local_dir/../build
+fi
 
-truffle migrate -f 2 --network "${NETWORK}"
+
+npm run  migrate -- --network "${NETWORK}"
 if [ $? -ne 0 ]; then
   echo "aborting"
   exit 1
--- truffle.js
+++ truffle.js
@@ -1,5 +1,7 @@
 // openzeppelin has a bunch of good helpers for testing and they need to be compiled to with babel as they use es2015 import.
 // TODO remove babel dependency when updating to v1.12.0 as they are not using babel anymore
+const toBuffer = require("ethereumjs-util").toBuffer;
+
 require("babel-register")({
     ignore: /node_modules\/(?!openzeppelin-solidity)/
 });
@@ -7,29 +9,36 @@ require('babel-polyfill');
 
 //TODO Use import keys and truffle-wallet-provider module to sign txs from the code and use in networks
 let account = process.env.MIGRATE_ADDRESS;
+let WalletProvider = require("truffle-wallet-provider");
+let endpoint = process.env.RINKEBY_PROVIDER || "http://127.0.0.1:8545"; //
+let privateKey = process.env.ETH_PRIVATE_KEY || "";
+
 
 module.exports = {
     networks: {
-      development: { // running against ganache + metamask default port
-        host: "localhost",
-        port: 8545,
-        network_id: "*", // Match any network id
-        from: account,
-        gas: 4712388
-      },
-      localgeth: {
-        host: "localhost",
-        port: 9545,
-        network_id: "*", // Match any network id
-        from: account,
-        gas: 4712388
-      },
-      rinkeby: {
-        host: "localhost",
-        port: 9545,
-        network_id: "4", // rinkeby network ID
-        from: "0x44a0579754d6c94e7bb2c26bfa7394311cc50ccb", // default address to use for any transaction Truffle makes during migrations
-        gas: 4612388 // Gas limit used for deploys
-      }
+        development: { // running against ganache + metamask default port
+            host: "localhost",
+            port: 8545,
+            network_id: "*", // Match any network id
+            from: account,
+            gas: 4712388
+        },
+        localgeth: {
+            host: "localhost",
+            port: 9545,
+            network_id: "*", // Match any network id
+            from: account,
+            gas: 4712388
+        },
+        rinkeby: {
+            provider: () => {
+                let wallet = require('ethereumjs-wallet').fromPrivateKey(toBuffer(privateKey));
+                return new WalletProvider(wallet, endpoint);
+            },
+            port: 9545,
+            network_id: "4", // rinkeby network ID
+            from: account, // 0x44a0579754d6c94e7bb2c26bfa7394311cc50ccb" default address to use for any transaction Truffle makes during migrations
+            gas: 4712388 // Gas limit used for deploys
+        }
     }
 };
