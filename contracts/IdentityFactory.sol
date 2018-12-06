6da7317126a7eb23c5b5ed0a9c6add23da1a9edb
razvan <razvan@centrifuge.io>
2018-12-06 17:10:39 +0100

    add createIdentityFor

--- contracts/IdentityFactory.sol
+++ contracts/IdentityFactory.sol
@@ -7,9 +7,23 @@ import "contracts/Identity.sol";
 contract IdentityFactory is Initializable {
 
   event IdentityCreated(address indexed centrifugeId);
+
+  /**
+  * Deploys a new identity and transfers the ownership to the sender
+  */
   function createIdentity() public {
     Identity identity = new Identity();
     identity.transferOwnership(msg.sender);
     emit IdentityCreated(identity);
+  }
+
+  /**
+  * Deploys a new identity and transfers the ownership to the provided address
+  * @param owner string address owner of the new identity
+  */
+  function createIdentityFor(address owner) public {
+    Identity identity = new Identity();
+    identity.transferOwnership(msg.sender);
+    emit IdentityCreated(owner);
   }
 }
--- test/IdentityFactory.test.js
+++ test/IdentityFactory.test.js
@@ -8,21 +8,31 @@ let identityFactoryContract;
 
 contract("IdentityFactory", function (accounts) {
 
-  before(async function () {
-    identityFactoryContract = await IdentityFactory.new();
-  });
-
-  describe("Create Identity", async function () {
-
-    it("should register identity and transfer ownership", async function () {
-      let createdAddress;
-      await identityFactoryContract.createIdentity({ from: accounts[1] }).then(function(tx) {
-        assertEvent(tx, "OwnershipTransferred", {newOwner: accounts[1]});
-        createdAddress = getEventValue(tx, "IdentityCreated", "centrifugeId");
-        assert.notEqual(web3.utils.hexToNumberString(createdAddress),"0")
-        assert.notEqual(web3.utils.hexToNumberString(createdAddress),"undefined")
-      });
+    before(async function () {
+        identityFactoryContract = await IdentityFactory.new();
     });
-  });
+
+    describe("Create Identity", async function () {
+
+        it("should register identity and transfer ownership to the sender", async function () {
+            let createdAddress;
+            await identityFactoryContract.createIdentity({from: accounts[1]}).then(function (tx) {
+                assertEvent(tx, "OwnershipTransferred", {newOwner: accounts[1]});
+                createdAddress = getEventValue(tx, "IdentityCreated", "centrifugeId");
+                assert.notEqual(web3.utils.hexToNumberString(createdAddress), "0")
+                assert.notEqual(web3.utils.hexToNumberString(createdAddress), "undefined")
+            });
+        });
+
+        it("should register identity and transfer ownership to the provided address", async function () {
+            let createdAddress;
+            await identityFactoryContract.createIdentityFor(accounts[2], {from: accounts[1]}).then(function (tx) {
+                assertEvent(tx, "OwnershipTransferred", {newOwner: accounts[2]});
+                createdAddress = getEventValue(tx, "IdentityCreated", "centrifugeId");
+                assert.notEqual(web3.utils.hexToNumberString(createdAddress), "0")
+                assert.notEqual(web3.utils.hexToNumberString(createdAddress), "undefined")
+            });
+        });
+    });
 
 });
