9fdc5577aa634ca6478a2019a48cd05ca4e2eab3
razvan <razvan@centrifuge.io>
2019-01-23 09:53:18 +0100

    change sol pragma

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,6 +1,6 @@
-pragma solidity 0.5.0;
+pragma solidity ^0.5.0;
 
-import "zos-lib/contracts/Initializable.sol";
+import "../node_modules/zos-lib/contracts/Initializable.sol";
 import "contracts/KeyManager.sol";
 import "contracts/lib/ECDSA.sol";
 
