4ed5a0d16b38dbceb8dadb65a6be11a95980875d
razvan <razvan@centrifuge.io>
2019-02-08 11:58:09 +0100

    reset migrations for zos deploy, add deployments folder to npm

--- scripts/pullContracts.sh
+++ scripts/pullContracts.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 if [ -z ${CENT_ETHEREUM_CONTRACTS_DIR} ]; then
     local_dir="$(dirname "$0")"
@@ -8,5 +8,8 @@ fi
 rm -Rf $local_dir/../build
 npm install @centrifuge/ethereum-contracts@latest --force --no-save
 mkdir -p $local_dir/../build/contracts
-stat -t -- local_dir/../node_modules/@centrifuge/ethereum-contracts/build/contracts/Migrations.json >/dev/null 2>&1  && cp -rf $local_dir/../node_modules/@centrifuge/ethereum-contracts/build/contracts/Migrations.json $local_dir/../build/contracts/
+stat -t -- $local_dir/../node_modules/@centrifuge/ethereum-contracts/build/contracts/Migrations.json >/dev/null 2>&1  && cp -R -p $local_dir/../node_modules/@centrifuge/ethereum-contracts/build/contracts/Migrations.json $local_dir/../build/contracts/
 stat -t -- $local_dir/../node_modules/@centrifuge/ethereum-contracts/zos*.json >/dev/null 2>&1 && cp -R -p $local_dir/../node_modules/@centrifuge/ethereum-contracts/zos*.json $local_dir/../
+
+
+
