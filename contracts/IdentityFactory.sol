61e302e2efba1dba5a7e6297eb1b5de15b5f0504
Razvan Dinicut <razvan.dinicut@gmail.com>
2019-02-26 17:48:50 +0100

    Audit refactoring  (#98)
    * fix some audit issues
    
    * refact variable name for erc721
    
    * refactor variable names
    
    * fix audit warnings
    
    * fix compilation
    
    * fix audit warnings
    
    * fix compilation
    
    * allow execute only for Action purpose and remove self from admin modifier
    
    * fix tests

--- contracts/IdentityFactory.sol
+++ contracts/IdentityFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 
 import "zos-lib/contracts/Initializable.sol";
 import "contracts/Identity.sol";
@@ -11,17 +11,23 @@ contract IdentityFactory is Initializable {
   /**
   * Deploys a new identity and transfers the ownership to the sender
   */
-  function createIdentity() public {
-    Identity identity = new Identity(msg.sender);
-    emit IdentityCreated(address(identity));
+  function createIdentity()
+  external
+  {
+    Identity identity_ = new Identity(msg.sender);
+    emit IdentityCreated(address(identity_));
   }
 
   /**
   * Deploys a new identity and transfers the ownership to the provided address
   * @param owner string address owner of the new identity
   */
-  function createIdentityFor(address owner) public {
-    Identity identity = new Identity(owner);
-    emit IdentityCreated(address(identity));
+  function createIdentityFor(
+    address owner
+  )
+  external
+  {
+    Identity identity_ = new Identity(owner);
+    emit IdentityCreated(address(identity_));
   }
 }
--- contracts/erc721/PaymentObligation.sol
+++ contracts/erc721/PaymentObligation.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 pragma experimental ABIEncoderV2;
 
 import "zos-lib/contracts/Initializable.sol";
@@ -16,7 +16,7 @@ contract PaymentObligation is Initializable, UserMintableERC721 {
   );
 
   // hardcoded supported fields for minting a PaymentObligation
-  bytes[] public mandatoryFields_;
+  bytes[] public _mandatoryFields;
 
   struct PODetails {
     bytes grossAmount;
@@ -24,7 +24,7 @@ contract PaymentObligation is Initializable, UserMintableERC721 {
     bytes dueDate;
   }
 
-  mapping(uint256 => PODetails) internal poDetails_;
+  mapping(uint256 => PODetails) internal _poDetails;
 
   /**
   * @dev Returns the values associated with a token
@@ -35,7 +35,7 @@ contract PaymentObligation is Initializable, UserMintableERC721 {
   * by the set up anchorRegistry
   * @param documentRoot bytes32 The root hash of the merkle proof/doc
   */
-  function getTokenDetails(uint256 _tokenId)
+  function getTokenDetails(uint256 tokenId)
   external
   view
   returns (
@@ -47,150 +47,150 @@ contract PaymentObligation is Initializable, UserMintableERC721 {
   )
   {
     return (
-      poDetails_[_tokenId].grossAmount,
-      poDetails_[_tokenId].currency,
-      poDetails_[_tokenId].dueDate,
-      tokenDetails_[_tokenId].anchorId,
-      tokenDetails_[_tokenId].rootHash
+      _poDetails[tokenId].grossAmount,
+      _poDetails[tokenId].currency,
+      _poDetails[tokenId].dueDate,
+      _tokenDetails[tokenId].anchorId,
+      _tokenDetails[tokenId].rootHash
     );
   }
 
 
   /**
-   * @param _anchorRegistry address The address of the anchor registry
+   * @param registry address The address of the anchor registry
    * that is backing this token's mint method.
    * that ensures that the sender is authorized to mint the token
    */
   function initialize(
-    address _anchorRegistry
+    address registry
   )
   public
   initializer
   {
     // compact property for "invoice.gross_amount",invoice = 1, gross_amount = 14
-    mandatoryFields_.push(hex"010000000000000e");
+    _mandatoryFields.push(hex"010000000000000e");
     // compact property for invoice.currency, invoice = 1, currency = 13
-    mandatoryFields_.push(hex"010000000000000d");
+    _mandatoryFields.push(hex"010000000000000d");
     // compact property for  invoice.due_date, invoice = 1, due_date = 22
-    mandatoryFields_.push(hex"0100000000000016");
+    _mandatoryFields.push(hex"0100000000000016");
 
     UserMintableERC721.initialize(
       "Centrifuge Payment Obligations",
       "CENT_PAY_OB",
-      _anchorRegistry,
-      mandatoryFields_
+      registry,
+      _mandatoryFields
     );
   }
 
   /**
    * @dev Mints a token after validating the given merkle proofs
    * and comparing it to the anchor registry's stored hash/doc ID.
-   * @param _to address The recipient of the minted token
-   * @param _tokenId uint256 The ID for the minted token
-   * @param _tokenURI string The metadata uri
-   * @param _anchorId uint256 The ID of the document as identified
+   * @param to address The recipient of the minted token
+   * @param tokenId uint256 The ID for the minted token
+   * @param tokenURI string The metadata uri
+   * @param anchorId uint256 The ID of the document as identified
    * by the set up anchorRegistry.
-   * @param _nextAnchorId uint256 The id that will be anchored when a change
+   * @param nextAnchorId uint256 The id that will be anchored when a change
    * is made to the document. It is not part of the values array in order
    * to avoid a bytes to uint conversion in the contract
-   * @param _properties bytes[] The properties of the leafs that are being proved
+   * @param properties bytes[] The properties of the leafs that are being proved
    * using precise-proofs
-   * @param _values bytes[] The values of the leafs that are being proved
+   * @param values bytes[] The values of the leafs that are being proved
    * using precise-proofs
-   * @param _salts bytes32[] The salts for the field that is being proved
+   * @param salts bytes32[] The salts for the field that is being proved
    * Will be concatenated for proof verification as outlined in
    * precise-proofs library.
-   * @param _proofs bytes32[][] Documents proofs that are needed
+   * @param proofs bytes32[][] Documents proofs that are needed
    * for proof verification as outlined in precise-proofs library.
    */
   function mint(
-    address _to,
-    uint256 _tokenId,
-    string memory _tokenURI,
-    uint256 _anchorId,
-    uint256 _nextAnchorId,
-    bytes[] memory _properties,
-    bytes[] memory _values,
-    bytes32[] memory _salts,
-    bytes32[][] memory _proofs
+    address to,
+    uint256 tokenId,
+    string memory tokenURI,
+    uint256 anchorId,
+    uint256 nextAnchorId,
+    bytes[] memory properties,
+    bytes[] memory values,
+    bytes32[] memory salts,
+    bytes32[][] memory proofs
   )
   public
   {
     // First check if the tokenId exists
     require(
-      !_exists(_tokenId),
+      !_exists(tokenId),
       "Token exists"
     );
 
     // Get the document root from AnchorRepository
-    bytes32 merkleRoot = super._getDocumentRoot(
-      _anchorId
+    bytes32 merkleRoot_ = super._getDocumentRoot(
+      anchorId
     );
 
     // Enforce that there is not a newer version of the document on chain
     super._requireIsLatestDocumentVersion(
-      merkleRoot,
-      _nextAnchorId,
-      _salts[3],
-      _proofs[3]
+      merkleRoot_,
+      nextAnchorId,
+      salts[3],
+      proofs[3]
     );
 
     // Verify that only one token per document/registry is minted
     super._requireOneTokenPerDocument(
-      merkleRoot,
-      _tokenId,
-      _salts[4],
-      _proofs[4]
+      merkleRoot_,
+      tokenId,
+      salts[4],
+      proofs[4]
     );
 
     // Check if document has a read rule defined
     bytes8 readRoleIndex = super._requireReadRole(
-      merkleRoot,
-      _properties[0],
-      _values[3],
-      _salts[5],
-      _proofs[5]
+      merkleRoot_,
+      properties[0],
+      values[3],
+      salts[5],
+      proofs[5]
     );
 
     // Check if the read rule has a read action
     super._requireReadAction(
-      merkleRoot,
+      merkleRoot_,
       readRoleIndex,
-      _salts[6],
-      _proofs[6]
+      salts[6],
+      proofs[6]
     );
 
     // Check if the token has the read role assigned to it
     super._requireTokenHasRole(
-      merkleRoot,
-      _tokenId,
-      _properties[1],
-      _values[3], // the value from read role proof
-      _salts[7],
-      _proofs[7]
+      merkleRoot_,
+      tokenId,
+      properties[1],
+      values[3], // the value from read role proof
+      salts[7],
+      proofs[7]
     );
 
     super._mintAnchor(
-      _to,
-      _tokenId,
-      _anchorId,
-      merkleRoot,
-      _tokenURI,
-      _values,
-      _salts,
-      _proofs
+      to,
+      tokenId,
+      anchorId,
+      merkleRoot_,
+      tokenURI,
+      values,
+      salts,
+      proofs
     );
 
-    poDetails_[_tokenId] = PODetails(
-      _values[0],
-      _values[1],
-      _values[2]
+    _poDetails[tokenId] = PODetails(
+      values[0],
+      values[1],
+      values[2]
     );
 
     emit PaymentObligationMinted(
-      _to,
-      _tokenId,
-      _tokenURI
+      to,
+      tokenId,
+      tokenURI
     );
   }
 
