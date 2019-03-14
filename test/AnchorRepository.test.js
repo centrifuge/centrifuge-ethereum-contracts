5292421f8215d80ceecc1fff4d1c6e7eeb6234cc
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-03-14 16:49:20 +0100

    Record blocknumber when anchoring

--- test/AnchorRepository.test.js
+++ test/AnchorRepository.test.js
@@ -117,7 +117,9 @@ contract("AnchorRepository", function (accounts) {
             assert.equal(bufferToHex(hashedAnchorId), web3.utils.toHex(response[0]));
             assert.equal(documentRoot, response[1]);
 
-
+            let blckNoResponse = await this.anchorRepository.getAnchoredBlockById.call(hashedAnchorId, callOptions);
+            assert.equal(bufferToHex(hashedAnchorId), web3.utils.toHex(blckNoResponse[0]));
+            assert.isAbove(0,  web3.utils.BN(blckNoResponse[1]));
         });
 
         it("should return an empty anchor for wrong anchorId", async function () {
