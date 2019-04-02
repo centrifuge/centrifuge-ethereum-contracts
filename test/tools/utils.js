d32a2a406129d48d513ac9b211f856161a22c57c
razvan <razvan@centrifuge.io>
2019-04-02 11:47:35 +0200

    test travis

--- test/tools/utils.js
+++
@@ -1,8 +0,0 @@
-const {bufferToHex, toBuffer,setLengthLeft} = require('ethereumjs-util');
-
-module.exports = {
-    addressToBytes32: (address) => {
-        return bufferToHex(setLengthLeft(toBuffer(address),32));
-    }
-};
-
